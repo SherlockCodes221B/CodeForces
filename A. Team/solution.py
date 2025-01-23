c13b4f64aeeef2d28b78c478f42a7be666f4afcd
Sherlock Holmes <shreeshastudy@gmail.com>
2025-01-23 11:41:00 +0530

    23/01/2025

--- A. Team/solution.py
+++ A. Team/solution.py
@@ -26,13 +26,11 @@ OutputCopy
 
 '''
 
-l=[]
-for i in range(0, int(input())):
-    j = input().split()
-    m=0
-
-    if j.count('2') >=2 :
-        m = m + 1
-        l.append(m)
-print(sum(l))
+g=int(input())
+c=0
+for i in range(g):
+    p,v,t=[int(x) for x in input().split()]
+    if p+v+t>=2:
+        c+=1
+print(c)
     
